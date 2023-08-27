b6f992b281cfdbc700d94b41e3eac96ff5bf52bb
Suprika Kottapalli <127580112+SuprikaKottapalli@users.noreply.github.com>
2023-08-27 15:37:56 +0530

    Add files via upload

---
+++ Swapping.py
@@ -0,0 +1,14 @@
+a = 5
+b = 10
+print("Before Swapping")
+print("a = ",a)
+print("b = ",b)
+# a , b = b , a
+
+
+a = a + b
+b = a - b
+a = a - b
+print("After Swapping")
+print("a = ",a)
+print("b = ",b)
